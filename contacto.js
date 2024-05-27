8dd2ca4512d088fdbfb4eb65542d05d76aa9304f
Candela Guadalupe Godoy <163220967+candelagodoy@users.noreply.github.com>
2024-05-27 17:59:21 -0300

    algo

---
+++ contacto.js
@@ -0,0 +1,19 @@
+const nom = document.getElementById("nombre");
+const apell = document.getElementById("apellido");
+const email = document.getElementById("email");
+const cel = document.getElementById("celular");
+const form = document.getElementById("form");
+
+
+
+
+function validar(){
+    
+    if ( form == ""){
+
+        alert("Debe completar el formulario para enviarlo");
+        return false;
+
+    }
+
+}
