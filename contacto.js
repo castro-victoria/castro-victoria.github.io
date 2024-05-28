561ba371d10cdc7eadcb85d3b6d1b75336ae7113
castro-victoria <mariavictoriacastro15@gmail.com>
2024-05-28 12:45:10 -0300

    validaciones

--- contacto.js
+++ contacto.js
@@ -1,14 +1,88 @@
-const nom = document.getElementById("nombre");
-const apell = document.getElementById("apellido");
-const email = document.getElementById("email");
-const cel = document.getElementById("celular");
-const form = document.getElementById("form");
+
+
+let nom = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let email = document.getElementById("email");
+let cel = document.getElementById("celular");
+//let opcion = document.getElementById("opcion");
+//let text = document.getElementById("mensaje")
+let lerr = document.getElementById("listaErrores");
+let lmen = document.getElementById("listaMensajes");
 
 
 
 
 function validar(){
+    lerr.innerHTML = "";
+    nom.classList.remove("error");
+    //nom.classList.remove("error");
+    //apellido.classList.remove("error");
+    //email.classList.remove("error");
+    //cel.classList.remove("error");
+    //opcion.classList.remove("error");
+    //text.classList.remove("error");
+
+    let errores = [];
+
+    let nbre = nom.value.trim();
+    if(nbre.lenght == 0){
+        errores.push("Falta ingresar el nombre");
+        nom.classList.add("Error");
+    } else if(nbre.lenght > 10){
+        errores.push("Nombre muy largo (10 carcteres)");
+        nom.classList.add("Error");
+    }
+
+    let apel = apellido.value.trim();
+    if(apel.lenght == 0){
+        errores.push("Falta ingresar el apellido");
+        apellido.classList.add("Error");
+    } else if(apel.lenght > 10){
+        errores.push("Nombre muy largo (10 carcteres)");
+        apellido.classList.add("Error");
+    }
+
+    let exre_cel = /^[1-9]\d{9}$/;
+    if(cel.value.length > 10) {
+        errores.push("Teléfono muy largo (10 dígitos)");
+        cel.classList.add("error");
+       }
+       if(cel.value.length > 0 && !exre_cel.test(cel.value)){
+        errores.push("Teléfono inválido (10 dígitos, no puede empezar con cero)");
+        cel.classList.add("Error");
+    }
+
+    let exre_email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    if(email.value.length > 25) {
+        errores.push("Email muy largo (25 dígitos)");
+        email.classList.add("Error");
+       }
+       if(email.value.length > 0 && !exre_email.test(email.value)){
+        errores.push("Email inválido (25 dígitos, no puede empezar con cero)");
+        email.classList.add("Error");
+    }
+
+
+
+
+
+
     
-    
+    for(let err of errores){
+        let li = document.createElement("li");
+        li.innerHTML = err;
+        lerr.appendChild(li);
+    }
+    if(errores.lenght == 0){
+        let li = document.createElement("li");
+        li.innerHTML = 'El formulario se envió exitosamente. Aguarde a su respuesta. Atte: Yoga Studio.'
+        lmen.appendChild(li);
+        document.forms[0].reset();
+        return false;
+    } else {
+        return false;
+    }
+
+
 
 }
