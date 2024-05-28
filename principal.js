0549bbb9e443d834a81656595346d94e1700c41d
Candela Guadalupe Godoy <163220967+candelagodoy@users.noreply.github.com>
2024-05-28 01:46:24 -0300

    carrusel y mas cositas

--- principal.js
+++ principal.js
@@ -1,53 +1,46 @@
-const botonLeft = document.querySelector(".btn-left");
-const botonRight = document.querySelector(".btn-right");
-const lado = document.querySelector("#slider");
-const ladoSection = document.querySelectorAll(".slider-section");
+let imagenes = [];
+imagenes[0] = "imagenes/1c.jpg"
+imagenes[1] = "imagenes/2c.jpg"
+imagenes[2] = "imagenes/3c.jpg"
+imagenes[3] = "imagenes/4c.jpg"
+imagenes[4] = "imagenes/5c.jpg"
+imagenes[5] = "imagenes/6c.jpg"
+imagenes[6] = "imagenes/7c.jpg"
+imagenes[7] = "imagenes/8c.jpg"
+imagenes[8] = "imagenes/9c.jpg"
 
+let x=0;
 
-botonLeft.addEventListener("click", e => moverLeft());
-botonRight.addEventListener("click", e => moverRight());
-
-let operacion = 0;
-let counter = 0;
-let widthImg = 100 / ladoSection.length;
-
-function moverRight() {
-    if (counter >= ladoSection.length-1) {
-        counter = 0;
-        operacion = 0;
-        lado.style.transform = `translate(-${operacion}%)`;
-        lado.style.transition = "none";
-        return;
-    } 
-    counter++;
-    operacion = operacion + widthImg;
-    lado.style.transform = `translate(-${operacion}%)`;
-    lado.style.transition = "all ease .6s";
-    
-}  
-
-function moverLeft() {
-    counter--;
-    if (counter < 0 ) {
-        counter = ladoSection.length-1;
-        operacion = widthImg * (ladoSection.length-1);
-        lado.style.transform = `translate(-${operacion}%)`;
-        lado.style.transition = "none";
-        return;
-    } 
-    operacion = operacion - widthImg;
-    lado.style.transform = `translate(-${operacion}%)`;
-    lado.style.transition = "all ease .6s";
-
-}  
-
-function mouseover(x){
-    x.style.height = "100%";
-    x.style.width = "100%";
+function next(){
+    x++;
+    let photo = document.getElementById("fotos");
+    if(x < imagenes.length)
+    {
+        photo.setAttribute("src",imagenes[x]);
+        console.log("entra if");
+    }
+    else
+    {
+        console.log("entra else");
+        x=0;
+        photo.setAttribute("src",imagenes[x]);
+   }
+ 
 }
 
-function normal(x){
-    x.style.height = "90%";
-    x.style.width = "90%";
+function prev(){
+    x--; 
+    let photo =  document.getElementById("fotos");
+    if(x >= 0)
+    {
+        photo.setAttribute("src",imagenes[x]);
+    }
+    else
+    {
+        x=[imagenes.length-1]
+        photo.setAttribute("src",imagenes[x]);
+    }
+}  
 
-}
+window.setInterval(next,4000);
+
