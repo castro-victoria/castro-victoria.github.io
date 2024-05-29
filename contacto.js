a8b6deb4d7743f4ac10132a3285ebd62a42f105b
Candela Guadalupe Godoy <163220967+candelagodoy@users.noreply.github.com>
2024-05-29 16:10:29 -0300

    ahora si

--- contacto.js
+++ contacto.js
@@ -4,17 +4,14 @@ let nom = document.getElementById("nombre");
 let apellido = document.getElementById("apellido");
 let email = document.getElementById("email");
 let cel = document.getElementById("celular");
-//let opcion = document.getElementById("opcion");
-//let text = document.getElementById("mensaje")
+let opcion = document.getElementById("opcion");
+let text = document.getElementById("mensaje");
 let lerr = document.getElementById("listaErrores");
-let lmen = document.getElementById("listaMensajes");
-
-
 
 
 function validar(){
     lerr.innerHTML = "";
-    nom.classList.remove("error");
+    //nom.classList.remove("error");
     //nom.classList.remove("error");
     //apellido.classList.remove("error");
     //email.classList.remove("error");
@@ -25,7 +22,7 @@ function validar(){
     let errores = [];
 
     let nbre = nom.value.trim();
-    if(nbre.lenght == 0){
+    if(nbre.length == 0){
         errores.push("Falta ingresar el nombre");
         nom.classList.add("Error");
     } else if(nbre.lenght > 10){
@@ -44,42 +41,53 @@ function validar(){
         
     }
 
-    let exre_cel = /^[1-9]\d{9}$/;
-    if(cel.value.length > 10) {
-        errores.push("Teléfono muy largo (10 dígitos)");
-        cel.classList.add("error");
-       }
-       if(cel.value.length > 0 && !exre_cel.test(cel.value)){
-        errores.push("Teléfono inválido (10 dígitos, no puede empezar con cero)");
-        cel.classList.add("Error");
-    }
 
     let exre_email = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
     let e = email.value.trim();
-    if(e.lenght == ""){
+    if(e.lenght == 0){
         errores.push("Falta ingresar el email");
         email.classList.add("Error");
-    } else if(e.length > 25){
-        errores.push("Email muy largo (25 dígitos)");
-        email.classList.add("Error");
-       }
-       if(email.value.length > 0 && !exre_email.test(email.value)){
+    } else if(e.length > 0 && !exre_email.test(e.value)){
         errores.push("Email inválido (25 dígitos, no puede empezar con cero)");
         email.classList.add("Error");
     }
 
+    let exre_cel = /^[1-9]\d{9}$/;
+    celu = celular.value.trim();
+    if(celu.length == 0){
+        errores.push("Falta ingresar el celular");
+        celular.classList.add("error");
+    }else if(celu.length > 0 && !exre_cel.test(celu)){
+        errores.push("Teléfono inválido (10 dígitos, no puede empezar con cero)");
+        celular.classList.add("Error");
+    }
 
+    
 
+    if(opcion.value == ""){
+        errores.push("Debe elegir una opcion");
+        opcion.classList.add("Error");
+    }
 
+    let text = mensaje.value.trim();
+    if(text.length == ""){
+        errores.push("Falta ingresar su mensaje");
+        mensaje.classList.add("Error");
+    } else if(text.lenght > 150){
+        errores.push("Mensaje muy largo");
+        mensaje.classList.add("Error");
+    }
 
 
     
+
+
     for(let err of errores){
         let li = document.createElement("li");
         li.innerHTML = err;
         lerr.appendChild(li);
     }
-    if(errores.lenght == 0){
+    if(errores.length == 0){
         let li = document.createElement("li");
         li.innerHTML = 'El formulario se envió exitosamente. Aguarde a su respuesta. Atte: Yoga Studio.'
         lmen.appendChild(li);
@@ -87,15 +95,6 @@ function validar(){
         return false;
 
     }
-    let men = 'Hola $(nbre), pronto estaremos en contacto contigo. Yoga Studio.';
-    pSaludo.innerHTML = men;
-    return false;
-
-}
-
-function erroresf(){
-    for(let err of errores){
-        console.log(err);
-    }
+    
 
 }
